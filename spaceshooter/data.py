b2fc660196325f2bce635311d51982fb22a361a6
Nylah D. Klasson <16917393+TxJson@users.noreply.github.com>
2019-08-06 12:58:59 +0200

    Made into package, renamed package folder, refactored code into a application

---
+++ spaceshooter/data.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class GameState(Enum):
+    NONE=0
+    QUIT=1
+    MENU=2
+    GAME=3
+    GAMEONLINE=4
+
+
+class MenuButton(Enum):
+    NONE=0
+    QUIT=1
+    PLAY=2
+    CONNECT=3
---
+++ spaceshooter/game.py
@@ -0,0 +1,32 @@
+#game.py
+
+from graphics import Point
+from graphics import color_rgb
+
+from player import Player
+
+from data import GameState
+
+import input
+
+class Game:
+    def __init__(self, win):
+        self.player = Player(Point(100, 100), win, 25, 450, color_rgb(0, 255, 0))
+        self.online = False #For peer-to-peer gameplay
+
+    def update(self, dt, win):
+        data = GameState.NONE
+
+        self.player.update(dt, win)
+
+        if input.kReturn():
+            print("Return to menu")
+            data = GameState.MENU
+
+        return data
+
+    def draw(self, win):
+        self.player.draw(win)
+
+    def clear(self):
+        self.player.clear()
